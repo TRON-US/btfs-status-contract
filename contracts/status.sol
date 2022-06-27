e6dbb548257bb059f0a3701723bac10f5d77d511
fish <920886811@163.com>
2022-06-27 16:19:52 +0800

    mod nowTime

--- contracts/status.sol
+++ contracts/status.sol
@@ -12,7 +12,7 @@ contract BtfsStatus {
     // info
     struct info {
         uint256 createTime;
-        string version;
+        bytes16 version;
         uint256 num;
         uint8[] hearts;
         uint256 lastNum;
@@ -22,10 +22,10 @@ contract BtfsStatus {
     mapping(string => info) private peerMap;
 
     //version
-    string public currentVersion;
+    bytes16 public currentVersion;
 
-    event versionChanged(string currentVersion, string version);
-    event statusReported(bytes32 merkleRootInput, uint256 index, address account, uint256 amount);
+    event versionChanged(bytes16 currentVersion, bytes16 version);
+    event statusReported(string peer, uint256 createTime, bytes16 version, uint256 num, uint256 nowTime, uint8[] hearts);
 
     //stat
     struct statistics {
@@ -44,16 +44,16 @@ contract BtfsStatus {
 
     // set current version
     // only owner do it
-    function setCurrentVersion(string memory ver) external {
-        string memory lastVersion = currentVersion;
+    function setCurrentVersion(bytes16 ver) external {
+        bytes16 lastVersion = currentVersion;
 
         currentVersion = ver;
         emit versionChanged(lastVersion, currentVersion);
     }
 
 
-    function setHeart(string memory peer, uint256 num, uint256 now) internal {
-        uint256 diffTime = now - peerMap[peer].lastTime;
+    function setHeart(string memory peer, uint256 num, uint256 nowTime) internal {
+        uint256 diffTime = nowTime - peerMap[peer].lastTime;
         if (diffTime > 30 * 86400) {
             diffTime = 30 * 86400;
         }
@@ -66,17 +66,17 @@ contract BtfsStatus {
         uint times = diffTime/86400;
         uint256 balance = diffNum;
         for (uint256 i = 1; i < times; i++) {
-            uint indexTmp = (now-i*86400)%86400%30;
+            uint indexTmp = (nowTime-i*86400)%86400%30;
             peerMap[peer].hearts[indexTmp] = uint8(diffNum/times);
 
             balance = balance - diffNum/times;
         }
 
-        uint index = now%86400%30;
+        uint index = nowTime%86400%30;
         peerMap[peer].hearts[index] = uint8(balance);
     }
 
-    function reportStatus(string memory peer, uint256 createTime, string memory version, uint256 num, uint256 now, bytes32 signed) external {
+    function reportStatus(string memory peer, uint256 createTime, bytes16 version, uint256 num, uint256 nowTime, bytes32 signed) external {
         require(0 < createTime, "reportStatus: Invalid createTime");
         require(0 < version.length, "reportStatus: Invalid version.length");
         require(0 < num, "reportStatus: Invalid num");
@@ -85,28 +85,33 @@ contract BtfsStatus {
         require(peerMap[peer].lastNum <= num, "reportStatus: Invalid lastNum<num");
 
         // Verify the signed with msg.sender.
-        bytes32 node = keccak256(abi.encodePacked(peer, createTime, version, num, now));
+        bytes32 node = keccak256(abi.encodePacked(peer, createTime, version, num, nowTime));
         require(verify(signed, node), "reportStatus: Invalid signed.");
 
-        uint index = now%86400%30;
+        uint index = nowTime%86400%30;
         peerMap[peer].createTime = createTime;
         peerMap[peer].version = version;
         peerMap[peer].lastNum = num;
 
         if (peerMap[peer].num == 0) {
-            peerMap[peer].hearts[index] = num;
+            if (num > 24) {
+                num = 24;
+            }
+            peerMap[peer].hearts[index] = uint8(num);
             totalStat.totalUsers += 1;
         } else {
-            setHeart(peer, num, now);
+            setHeart(peer, num, nowTime);
         }
 
         // set total
         totalStat.total += 1;
 
         emit statusReported(
-            peerMap[peer].createTime,
-            peerMap[peer].version,
-            peerMap[peer].lastNum,
+            peer,
+            createTime,
+            version,
+            num,
+            nowTime,
             peerMap[peer].hearts
         );
     }
